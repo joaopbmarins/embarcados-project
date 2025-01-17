6305cd893aceaf9d7864e75b07e387b609a235d0
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2025-01-17 12:08:36 -0300

    add intr and read gyro

--- lib/odometria/odometria.cpp
+++ lib/odometria/odometria.cpp
@@ -1,32 +1,61 @@
 #include "odometria.h"
 #include <cmath>
 
-odometria::odometria(PinName SDA, PinName SCL) : _MPU(SDA, SCL){
+odometria::odometria(int freq_p,PinName SDA, PinName SCL) : _MPU(SDA, SCL){
+    freq = freq_p;
+
     _MPU.initialize();
     time.start();
-    gyro[0] = 0.0;
-    gyro[1] = 0.0;
-    gyro[2] = 0.0;
-    tolerance = 2000.0 * 0.03;
-    speed.count = 0;
-    speed.speed_sum = 0.0;
+
+    intr_update_gyro.attach(callback(this, &odometria::update_gyro), std::chrono::microseconds(freq));
 }
 
-static double graus_to_rad(double graus){
+static double graus_to_rad(double &graus){
     return graus * (M_PI/180.0);
 }
 
-static void graus3_to_rad3(double *graus){
-    graus[0] = graus[0] * (M_PI/180.0);
-    graus[1] = graus[1] * (M_PI/180.0);
-    graus[2] = graus[2] * (M_PI/180.0);
+static void graus3_to_rad3(double graus[]){
+    graus[0] = graus_to_rad(graus[0]);
+    graus[1] = graus_to_rad(graus[1]);
+    graus[2] = graus_to_rad(graus[2]);
     return;
 }
 
 double odometria::read_gyro(){
-    this->_MPU.readGyro(gyro);
-    if(fabs(gyro[0]) > tolerance || fabs(gyro[1]) > tolerance || gyro[2] > tolerance){// se o valor absoluto lido de cada eixo for maior que a tolerancia
-
+    double gyro_new_read[3];
+    double gyro_diff[3];
+    this->_MPU.readGyro(gyro_new_read);
+    graus3_to_rad3(gyro_new_read);
+    for(int i=0;i<3;i++){
+        gyro_new_read[i] = roundl(gyro_new_read[i] * 100) / 100;
+        gyro_diff[i] = gyro[i] - gyro_new_read[i];
     }
 
+    if(fabs(gyro_diff[0]) >= tolerance || fabs(gyro_diff[1]) >= tolerance || gyro_diff[2] >= tolerance){// se o valor absoluto lido de cada eixo for maior que a tolerancia
+        gyro[0] = gyro_new_read[0];
+        gyro[1] = gyro_new_read[1];
+        gyro[2] = gyro_new_read[2];
+        
+        speed = gyro[2];
+    }
+
+}
+
+void odometria::update_gyro(){
+    read_gyro();
+    distance_total += speed * (std::chrono::duration_cast<std::chrono::microseconds>(time.elapsed_time()).count());
+    time.reset();//reinicia a contagem entre leituras
+
+}
+
+double odometria::speed_read(){
+    return this->speed;
+}
+void odometria::gyro_read(double destino[]){
+    destino[0] = gyro[0];
+    destino[1] = gyro[1];
+    destino[2] = gyro[2];
+}
+double odometria::distance_read(){
+    return this->distance_total;
 }
--- lib/odometria/odometria.h
+++ lib/odometria/odometria.h
@@ -9,23 +9,28 @@ typedef struct{
     size_t count;
 }Speed;
 
-static double graus_to_rad(double graus);
-static void graus3_to_rad3(double *graus);
+static double graus_to_rad(double &graus);
+static void graus3_to_rad3(double graus[]);
 
 
 class odometria{
     public:
-    odometria(PinName SDA, PinName SCL);
+    odometria(int freq_p, PinName SDA, PinName SCL);
     double read_gyro();
+    void update_gyro();
+    double speed_read();
+    void gyro_read(double destino[]);
     double distance_read();
 
     private:
     MPU6050 _MPU;
     Timer time;
-    Speed speed;
-    double distance;//rad
-    double gyro[3];
-    double tolerance;
+    Ticker intr_update_gyro;
+    int freq;
+    double speed = 0.0;// w speed
+    double distance_total = 0.0;//rad
+    double tolerance = 0.01;//rad/s
+    double gyro[3] = {0.0, 0.0, 0.0}; //{x,y,w}
 
 };
 
